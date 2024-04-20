8dcd6d4241391d69bac3622f2ec652d070d930a7
Limon <limonreza3.1416@gmail.com>
2024-04-20 10:37:28 +0600

    custom progress bar done

---
+++ app/src/main/java/com/torchtech/customprogressbar/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.torchtech.customprogressbar
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val button = findViewById<Button>(R.id.button)
+
+        var currentProgress= 0
+
+        button.setOnClickListener {
+            currentProgress += 10
+            progressBar.progress = currentProgress
+            progressBar.max=100
+        }
+    }
+}
